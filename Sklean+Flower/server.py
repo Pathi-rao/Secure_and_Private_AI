07bc5193ffb2088a255e56767e0af833fa7a3ddc
Pathi-rao <lakshmipathi0000@gmail.com>
2021-08-26 15:30:52 +0200

    Update server.py

--- Sklean+Flower/server.py
+++ Sklean+Flower/server.py
@@ -1,7 +1,10 @@
 import flwr as fl
 import utils
+import numpy as np
 from sklearn.metrics import log_loss
 from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import OneHotEncoder
+
 from typing import Dict
 
 import datahandler as dh
@@ -22,7 +25,10 @@ def get_eval_fn(model: LogisticRegression):
     def evaluate(parameters: fl.common.Weights):
         # Update model with the latest parameters
         utils.set_model_params(model, parameters)
-        loss = log_loss(y_test, model.predict_proba(X_test))
+        pred_proba = model.predict_proba(X_test)
+        print(pred_proba)
+        print(len(np.unique(y_test)))
+        loss = log_loss(y_test, pred_proba)
         accuracy = model.score(X_test, y_test)
         return loss, {"accuracy": accuracy}
 
@@ -32,6 +38,7 @@ def get_eval_fn(model: LogisticRegression):
 model = LogisticRegression()
 utils.set_initial_params(model)
 strategy = fl.server.strategy.FedAvg(
+    # FedAvg is a strategy for orchestrating the learning process on the server side.
     min_available_clients=2,
     eval_fn=get_eval_fn(model),
     on_fit_config_fn=fit_round,
